7b9309020321417ab97a42754eaefd48c75c1d35
Zheshard <antonzestkov489@gmail.com>
2024-03-26 11:05:54 +0300

    task 1 complete

--- homework_1/1.js
+++ homework_1/1.js
@@ -18,3 +18,39 @@
 // Итератор должен перебирать альбомы по порядку.
 
 // • Используйте цикл for...of для перебора альбомов в музыкальной коллекции и вывода их на консоль в формате: Название альбома - Исполнитель (Год выпуска)
+
+const musicCollection = {
+    albums: [
+        {
+            title: "Албьом 1",
+            artist: "Исполнитель 1",
+            year: "2000",
+        },
+        {
+            title: "Албьом 2",
+            artist: "Исполнитель 2",
+            year: "2001",
+        },
+        {
+            title: "Албьом 3",
+            artist: "Исполнитель 3",
+            year: "2002",
+        },
+    ],
+
+    [Symbol.iterator]: function () {
+        return {
+            current: 0,
+            albums: this.albums,
+            next() {
+                return this.current < this.albums.length
+                    ? { done: false, value: this.albums[this.current++] }
+                    : { done: true };
+            },
+        };
+    },
+};
+
+for (const album of musicCollection) {
+    console.log(`${album.title} - ${album.artist} (${album.year})`);
+}
