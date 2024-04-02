31415a8667dde9c1839320fd1ea2e71e1a92aaa8
Zheshard <antonzestkov489@gmail.com>
2024-04-02 19:58:39 +0300

    homework 2 complete

--- homework_2/2.js
+++ homework_2/2.js
@@ -7,7 +7,8 @@
 // где будут отображаться отзывы.
 
 // Напишите функцию, которая будет добавлять отзыв в контейнер с отзывами.
-// Однако если длина введенного отзыва менее 50 или более 500 символов, функция должна генерировать исключение.
+// Однако если длина введенного отзыва менее 50 или более 500 символов,
+// функция должна генерировать исключение.
 
 // При добавлении отзыва, он должен отображаться на странице под предыдущими отзывами, а не заменять их.
 
@@ -46,3 +47,59 @@
 // ];
 
 // Вы можете использовать этот массив initialData для начальной загрузки данных при запуске вашего приложения.
+
+const initialData = [
+    {
+        product: "Apple iPhone 13",
+        reviews: [
+            {
+                id: "1",
+                text: "Отличный телефон! Батарея держится долго.",
+            },
+            {
+                id: "2",
+                text: "Камера супер, фото выглядят просто потрясающе.",
+            },
+        ],
+    },
+    {
+        product: "Samsung Galaxy Z Fold 3",
+        reviews: [
+            {
+                id: "3",
+                text: "Интересный дизайн, но дорогой.",
+            },
+        ],
+    },
+    {
+        product: "Sony PlayStation 5",
+        reviews: [
+            {
+                id: "4",
+                text: "Люблю играть на PS5, графика на высоте.",
+            },
+        ],
+    },
+];
+
+function addReview() {
+    const reviewInput = document.getElementById("reviewInput").value;
+    if (reviewInput.length < 50 || reviewInput.length > 500) {
+        throw new Error("Длина отзыва должна быть от 50 до 500 символов.");
+    }
+
+    const reviewsContainer = document.getElementById("reviewsContainer");
+    const newReview = document.createElement("p");
+    newReview.textContent = reviewInput;
+    reviewsContainer.appendChild(newReview);
+}
+
+initialData.forEach((item) => {
+    const reviewsContainer = document.getElementById("reviewsContainer");
+    const productReviews = item.reviews;
+    productReviews.forEach((review) => {
+        const newReview = document.createElement("p");
+        newReview.textContent = review.text;
+        reviewsContainer.appendChild(newReview);
+    });
+});
